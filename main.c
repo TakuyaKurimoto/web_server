d18bbebc6ff0cd4cb4bffbabc01ab7ad9769de09
takuyaK <welcom4869@icloud.com>
2022-07-16 19:25:36 +0900

    sock再利用

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <jansson.h>
 #include <assert.h>
 #include "picohttpparser/picohttpparser.h"
+#include "include/uthash.h"
 
 #define BUF_LEN 256             /* バッファのサイズ */
 #define TRUE             1
@@ -30,24 +31,38 @@ const char *method, *path;
 int pret, minor_version;
 struct phr_header headers[100];
 size_t buflen, prevbuflen, method_len, path_len, num_headers;
-ssize_t rc; 
+ssize_t rc;
+struct my_struct {
+  int key;
+  int value;
+
+  UT_hash_handle hh;
+};
+struct my_struct *table = NULL;
+struct my_struct *item;
 
 void _closeConnection(int descriptor, int type){
    printf("close connection%d\n", descriptor);
    close(descriptor);
+   
    switch (type){
       case CLIENT:
          epoll_ctl(client_epollfd, EPOLL_CTL_DEL, descriptor, NULL);
          break;
       case SERVER:
          epoll_ctl(server_epollfd, EPOLL_CTL_DEL, descriptor, NULL);
+         HASH_FIND_INT(table, &(descriptor_array[descriptor_array[descriptor].num].num), item);
+         HASH_DEL(table, item);
+         printf("freeソケットリストからsock=%dを削除\n", descriptor);
+         free(item);
          break;
       default:
          epoll_ctl(epollfd, EPOLL_CTL_DEL, descriptor, NULL);
    }
 
    //mutex必要かも
-   descriptor_array[descriptor_array[descriptor].num].status = CLOSE;
+   descriptor_array[descriptor].status = CLOSE;
+   // descriptor_array[descriptor_array[descriptor].num].status = CLOSE;
 }
 
 /**
@@ -85,37 +100,45 @@ void _makeNonBlocking(int descriptor){
 }
 
 void send_http_request(int descriptor) {
-
    if (!descriptor_array[descriptor].status)
    {
-      int sock;
-      if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) perror("socketに失敗");
-
-      struct sockaddr_in addr;
-      addr.sin_family = AF_INET;
-      addr.sin_addr.s_addr = ip_addr;
-      addr.sin_port = htons(port);
-
-         
-      
-      if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) printf("connectに失敗 errno = %d\n", errno);
-      printf("サーバーとコネクト成功。descriptor = %d\n", sock);
-      _makeNonBlocking(sock);
-      
-      ev.events = EPOLLIN | EPOLLET;
-      ev.data.fd = sock;
-      if (epoll_ctl(server_epollfd, EPOLL_CTL_ADD, sock, &ev) == -1) {
-          perror("epoll_ctl: conn_sock");
-          exit(EXIT_FAILURE);
+      int sock = -1; 
+      for (item = table; item != NULL; item = item->hh.next) {
+         sock = item->key;
+         HASH_DEL(table, item);
+         printf("freeソケットリストからsock=%dを削除\n", sock);
+         free(item);
+         printf("まだ生きてるソケット再利用=%d\n", sock);
+         break;
       }
       
+      if (sock == -1){
+         if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) perror("socketに失敗");
+
+         struct sockaddr_in addr;
+         addr.sin_family = AF_INET;
+         addr.sin_addr.s_addr = ip_addr;
+         addr.sin_port = htons(port);
+
+         
+         if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) printf("connectに失敗 errno = %d\n", errno);
+         printf("サーバーとコネクト成功。descriptor = %d\n", sock);
+         _makeNonBlocking(sock);
+         
+         ev.events = EPOLLIN | EPOLLET;
+         ev.data.fd = sock;
+         if (epoll_ctl(server_epollfd, EPOLL_CTL_ADD, sock, &ev) == -1) {
+            perror("epoll_ctl: conn_sock");
+            exit(EXIT_FAILURE);
+         }
+      }
       descriptor_array[sock].num = descriptor;
       descriptor_array[sock].is_from_server = 0;
       descriptor_array[sock].status = OPEN;
       descriptor_array[descriptor].num = sock;
       descriptor_array[descriptor].status = OPEN;
-   }
-   if (send(descriptor_array[descriptor].num, buffer, strlen(buffer), 0) == -1) perror("sendに失敗");
+      }
+   if (send(descriptor_array[descriptor].num, buffer, strlen(buffer), 0) == -1) printf("sendに失敗。sock=%d\n", descriptor_array[descriptor].num);
    printf("サーバーに%ldバイト転送した\n", strlen(buffer));
   
 }
@@ -173,6 +196,13 @@ void * getDataFromServerAndSendDataToClient(void *arg){
                   perror("  recv() failed");
                }
                else{
+                  // 要素の追加
+                  item = malloc(sizeof(struct my_struct));
+                  item->key = sock;
+                  item->value = 1;
+                  HASH_ADD_INT(table, key, item);
+                  printf("freeソケットリストにsock=%dを追加\n", sock);
+                  descriptor_array[descriptor_array[sock].num].status = CLOSE;
                   printf("読み込めるデータがない\n");
                }
                break;
@@ -186,8 +216,7 @@ void * getDataFromServerAndSendDataToClient(void *arg){
             }
 
             printf("RECEIVE %d バイト \n",rc);
-
-            err = send(descriptor_array[sock].num, buf, strlen(buf), 0); 
+            err = send(descriptor_array[sock].num, buf, strlen(buf), 0);
             if (err < 0){
                perror("  send() failed");
             }
@@ -267,6 +296,7 @@ void * getDataFromClientAndSendDataToServer(void *arg){
             if (pret > 0)
                break; /* successfully parsed the request */
          }
+         /*
          if(rc != 0) {
             printf("request is %d bytes long\n", pret);
             printf("method is %.*s\n", (int)method_len, method);
@@ -277,7 +307,7 @@ void * getDataFromClientAndSendDataToServer(void *arg){
                printf("%.*s: %.*s\n", (int)headers[i].name_len, headers[i].name,
                      (int)headers[i].value_len, headers[i].value);
             }
-         }
+         }*/
       }
    } 
 }
